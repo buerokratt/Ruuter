7302ecd5688e2f55eceb11d7d09cd5967f8e559c
henrik.prangel <henrik.prangel@netgroup.ee>
2022-05-26 14:46:45 +0300

    chore: Add project base

---
+++ src/main/java/ee/ria/ruuter/RuuterApplication.java
@@ -0,0 +1,13 @@
+package ee.ria.ruuter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuuterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuuterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ee/ria/ruuter/RuuterApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.ria.ruuter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuuterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
