5a499628641f1adfeafa60e3d904f8cb0f6b14de
Ewert Ubaleht <ewert.ubaleht@netgroup.com>
2022-05-31 10:44:58 +0300

    feature: read configuration files and save them to an object

--- src/main/java/ee/ria/ruuter/configuration/routing/Configuration.java
+++ src/main/java/ee/ria/ruuter/configuration/routing/Configuration.java
@@ -5,6 +5,7 @@ import ee.ria.ruuter.service.ConfigurationNodeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
