7222d25456e6d06cd17a9e118e513e36dae48f29
Alar Floren <alar.floren@bpw-consulting.com>
2023-06-16 13:12:52 +0300

    #170 Ruuter GET request bugfix

--- src/main/java/ee/buerokratt/ruuter/helper/HttpHelper.java
+++ src/main/java/ee/buerokratt/ruuter/helper/HttpHelper.java
@@ -9,13 +9,17 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.netty.http.client.HttpClient;
 
 import java.time.Duration;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -42,19 +46,19 @@ public class HttpHelper {
     }
 
     public ResponseEntity<Object> doGet(String url, Map<String, Object> query, Map<String, String> headers) {
-        WebClient client = WebClient.builder()
-            .clientConnector(new ReactorClientHttpConnector(getHttpClient()))
-            .baseUrl(url)
-            .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-            .defaultUriVariables(query)
-            .build();
-
         try {
-            return client.get()
+            MultiValueMap<String, String> qp = new LinkedMultiValueMap<>(
+                query.entrySet().stream().collect(Collectors.toMap(e -> e.getKey(), e-> Arrays.asList(e.getValue().toString()))));
+            return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(getHttpClient())).build()
+                .get()
+                .uri(url, uriBuilder -> uriBuilder.queryParams(qp).build())
                 .headers(httpHeaders -> addHeadersIfNotNull(headers, httpHeaders))
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .retrieve()
                 .toEntity(Object.class)
                 .block();
+
         } catch (WebClientResponseException e) {
             return new ResponseEntity<>(e.getStatusText(), e.getStatusCode());
         }
