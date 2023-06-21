26a1087b05685fa4e47965208428e608b0f012ed
Alar Floren <alar.floren@bpw-consulting.com>
2023-06-21 17:09:48 +0300

    #170 Ruuter GET request parameter passing bugfix - fixed ScriptingHelper

--- src/main/java/ee/buerokratt/ruuter/helper/ScriptingHelper.java
+++ src/main/java/ee/buerokratt/ruuter/helper/ScriptingHelper.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Service;
 import javax.script.Bindings;
 import javax.script.ScriptEngine;
 import java.util.*;
-import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
 
 import static java.util.stream.Collectors.*;
@@ -31,43 +30,29 @@ public class ScriptingHelper {
     }
 
     public Object evaluateScripts(Object toEval, Map<String, Object> context, Map<String, Object> requestBody, Map<String, Object> requestQuery, Map<String, String> requestHeaders) {
+        return replaceVariables(toEval, context, requestBody, requestQuery, requestHeaders);
+    }
+
+    private Object replaceVariables(Object toEval, Map<String, Object> context, Map<String, Object> requestBody, Map<String, Object> requestQuery, Map<String, String> requestHeaders ) {
+        // Null evaluation: value does not contain any scriptable element or is empty
         if (toEval == null || !containsScript(toEval.toString())) {
             return toEval;
         }
 
-        Map<String, Object> evalContext = setupEvalContext(context, requestBody, requestQuery, requestHeaders);
-        Bindings bindings = createBindingsWithContext(evalContext);
-
-        Object ret = replaceVariables(toEval, bindings, evalContext);
-        return ret;
-    }
-
-    private Object replaceVariables(Object toEval, Bindings bindings, Map<String, Object> evalContext ) {
-        if (toEval instanceof String) {
-            String eval = (String) toEval;
-
-            // Full variable match, assigned value can be any
-            if (eval.matches(SCRIPT_REGEX)) {
-                setupObjectsInScript(eval, bindings, evalContext);
-                return filterEmptyOptional(bindings, eval.substring(2, eval.length() - 1));
-            }
-
-            // Partial variable match, assigned value can only be String
-            List<MatchResult> founded = Pattern.compile(SCRIPT_REGEX).matcher(eval).results().collect(toList());
-            for (MatchResult f : founded) {
-                String found = f.group();
-                setupObjectsInScript(found.substring(2, found.length() - 1), bindings, evalContext);
-                eval = eval.replace(found, filterEmptyOptional(bindings, found.substring(2, found.length() - 1)).toString());
-            };
-
-            return eval;
+        // Simple evaluation: value is one variable only
+        if (toEval.toString().matches(SCRIPT_REGEX)) {
+            return evaluateSimple(toEval, context, requestBody, requestQuery, requestHeaders);
         }
+
+        // Recursive evaluation: value is of complex type (List/Map)
         if (toEval instanceof List)
-            return ((List)toEval).stream().map( obj -> replaceVariables(obj, bindings, evalContext)).collect(toList());
+            return ((List)toEval).stream().map( obj -> replaceVariables(obj, context, requestBody, requestQuery, requestHeaders)).collect(toList());
         if (toEval instanceof Map)
             return ((HashMap<String, Object>)toEval).entrySet().stream()
-                .collect(toMap(Map.Entry::getKey,entry -> replaceVariables(entry.getValue(), bindings,evalContext)));
-        return null;
+                .collect(toMap(Map.Entry::getKey,entry -> replaceVariables(entry.getValue(), context, requestBody, requestQuery, requestHeaders)));
+
+        // Complex evaluation: value is a string formula
+        return evaluateComplex(toEval, context, requestBody, requestQuery, requestHeaders);
     }
 
     private Object filterEmptyOptional(Bindings bindings, String evaluableScript) {
@@ -137,4 +122,33 @@ public class ScriptingHelper {
     private String removeScriptWrapper(String s) {
         return s.substring(2, s.length() - 1);
     }
+
+    public Object evaluateSimple(Object toEval, Map<String, Object> context, Map<String, Object> requestBody, Map<String, Object> requestQuery, Map<String, String> requestHeaders) {
+        List<Object> evaluatedScripts = getEvaluatedScripts(toEval, context, requestBody, requestQuery, requestHeaders);
+        return evaluatedScripts.size() == 1 ? evaluatedScripts.get(0) : evaluatedScripts.stream().reduce("", (o, o2) -> o + o2.toString());
+    }
+
+    public Object evaluateComplex(Object toEval, Map<String, Object> context, Map<String, Object> requestBody, Map<String, Object> requestQuery, Map<String, String> requestHeaders) {
+        List<Object> evaluatedScripts = getEvaluatedScripts(toEval, context, requestBody, requestQuery, requestHeaders);
+        List<String> nonScriptSlices = Arrays.stream(toEval.toString().split(SCRIPT_REGEX)).toList();
+
+        if (nonScriptSlices.isEmpty()) {
+            return evaluatedScripts.size() == 1 ? evaluatedScripts.get(0) : evaluatedScripts.stream().reduce("", (o, o2) -> o + o2.toString());
+        }
+        return nonScriptSlices.stream()
+            .map(nonScriptSlice -> evaluatedScripts.isEmpty() ? nonScriptSlice : nonScriptSlice + evaluatedScripts.remove(0))
+            .reduce("", (s, s2) -> s + s2);
+    }
+
+    private List<Object> getEvaluatedScripts(Object toEval, Map<String, Object> context, Map<String, Object> requestBody, Map<String, Object> requestQuery, Map<String, String> requestHeaders) {
+        Map<String, Object> evalContext = setupEvalContext(context, requestBody, requestQuery, requestHeaders);
+        Bindings bindings = createBindingsWithContext(evalContext);
+        List<Object> evaluatedScripts = Pattern.compile(SCRIPT_REGEX, Pattern.MULTILINE).matcher(toEval.toString()).results()
+            .map(matchResult -> matchResult.group(0))
+            .map(scriptToExecute -> setupObjectsInScript(removeScriptWrapper(scriptToExecute), bindings, evalContext))
+            .map(evaluableScript -> filterEmptyOptional(bindings, evaluableScript))
+            .collect(toList());
+        return evaluatedScripts;
+    }
+
 }
